afe3f5d33373850ba9693c4e215ea40ce81c8530
diwebin-id <diwebinaja.company@gmail.com>
2025-02-11 17:52:52 +0700

    upload videos #1 - #6

--- data/Videos.js
+++ data/Videos.js
@@ -1,7 +1,10 @@
 const videos = [
-  // "XgVq9x6yKzs?si=RbWun2A1iHvQLa0Y",
-  // "25ROFXjoaAU?si=CT-s3B2_AjSFLGNQ",
-  // "bxPf53OQg14?si=o2z306cFFjxncTCy",
+  "hS-g2SRMkUQ?si=z-j8GZuuI2PYwM5I",
+  "zSeUIBVgqvE?si=qQu1oiodIAT_SnHv",
+  "ShNm-uQEcA8?si=XZxW1sC-RzdsoyZC",
+  "o2l-r212J4Q?si=ZNFs9g_m4I-GUYrQ",
+  "xzMw_LNhQz4?si=gTVIZ3yP2LS_XJnZ",
+  "eYSr3Nlxxuc?si=onK5he2dkYstIx91",
 ];
 
 module.exports = videos;
