bfb783676e230b3bcb4af8b120bb5efda51ff4f8
diwebin-id <diwebinaja.company@gmail.com>
2025-02-11 17:30:38 +0700

    delete DS #1

--- data/Design.js
+++ data/Design.js
@@ -1,25 +1,25 @@
 const dataDs = [
-  {
-    id: "Ds-001",
-    type: "Design",
-    images: [
-      "https://i.pinimg.com/736x/66/fd/32/66fd32b634a37484fc23944c4aea2371.jpg",
-      "https://i.pinimg.com/736x/f7/4d/7e/f74d7e63f8ea7918e96c80a1c18cf2c9.jpg",
-      "https://i.pinimg.com/736x/d9/13/ba/d913bad0cb81608280a6806462762749.jpg",
-      "https://i.pinimg.com/736x/28/63/de/2863de0f47b4a02bc497307917b800f8.jpg",
-    ],
-    title: "Projek 1 Design",
-    dataLink: {
-      online: "https://id.pinterest.com/",
-      github: "sfdsfdsf",
-      figma: "",
-    },
-    start: "2006-01-25",
-    end: "2010-01-24",
-    tech: ["React", "Vue", "Express"],
-    demoCode: "https://www.youtube.com/watch?v=xoBgzeQWqhU",
-    projectConnect: ["id-project-yang-terhubung"],
-  },
+  // {
+  //   id: "Ds-001",
+  //   type: "Design",
+  //   images: [
+  //     "https://i.pinimg.com/736x/66/fd/32/66fd32b634a37484fc23944c4aea2371.jpg",
+  //     "https://i.pinimg.com/736x/f7/4d/7e/f74d7e63f8ea7918e96c80a1c18cf2c9.jpg",
+  //     "https://i.pinimg.com/736x/d9/13/ba/d913bad0cb81608280a6806462762749.jpg",
+  //     "https://i.pinimg.com/736x/28/63/de/2863de0f47b4a02bc497307917b800f8.jpg",
+  //   ],
+  //   title: "Projek 1 Design",
+  //   dataLink: {
+  //     online: "https://id.pinterest.com/",
+  //     github: "sfdsfdsf",
+  //     figma: "",
+  //   },
+  //   start: "2006-01-25",
+  //   end: "2010-01-24",
+  //   tech: ["React", "Vue", "Express"],
+  //   demoCode: "https://www.youtube.com/watch?v=xoBgzeQWqhU",
+  //   projectConnect: ["id-project-yang-terhubung"],
+  // },
 ];
 
 module.exports = dataDs;
